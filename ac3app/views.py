fb8d4e90979d7bb66fd78ef50166ad9b030888fe
Chris <mackattackster@gmail.com>
2014-11-04 10:11:19 -0500

    Changed email reference in views. Added logic in MainView.html to check if the event image url is empty.

--- ac3app/views.py
+++ ac3app/views.py
@@ -6,7 +6,7 @@ from django.core import serializers
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
-import emails
+from ac3app import emails
 
 
 def index(request):
